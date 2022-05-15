2f9c61714d627f0aca1151d3f49668d2ff0d5f12
Emmanuel Ugwu <>
2022-05-15 18:29:47 +0100

    made changes

--- src/components/video-player.js
+++ src/components/video-player.js
@@ -32,18 +32,12 @@ const VideoPlayer = () => {
         src="https://player.cloudinary.com/embed/?public_id=milkshake&cloud_name=ugwutotheeshoes&player%5Bfluid%5D=true&player%5Bcontrols%5D=true&source%5BsourceTypes%5D%5B0%5D=mp4"
         width="640"
         height="480"
-        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+        controls
+        muted
+        analytics={true}
+        id="video-player"
         allowfullscreen
         frameborder="0"
-        analytics={{
-          events: [
-            "play",
-            "pause",
-            "ended",
-            { type: "percentsplayed", percents: [10, 40, 70, 90] },
-            "error",
-          ],
-        }}
       ></iframe>
     </div>
   );
