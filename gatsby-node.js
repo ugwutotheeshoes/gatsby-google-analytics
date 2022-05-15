607df9363a32675958ba5fd0154b486fc435fbce
Emmanuel Ugwu <>
2022-05-15 21:15:46 +0100

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+const path = require("path");
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  });
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /cld-video-player/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
